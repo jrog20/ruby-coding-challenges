4367befe39671d0dab8566c152d565b6bfbd4b34
Jaime Rogstad <jaimerogstad@gmail.com>
2022-01-09 16:11:13 -0500

    Fix function naming convention

--- find-average-of-subarray/find-average-of-subarray.js
+++ find-average-of-subarray/find-average-of-subarray.js
@@ -1,25 +1,25 @@
-// Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
+// Given an array, find the average of all subarrays of ‘k’ contiguous elements in it.
 
+// Brute force solution - If k = 5, this function will calculate the sum of every 
+// 5-element subarray of the given array and divide the sum by ‘5’ to find the average. 
 
-// Brute force solution - will calculate the sum of every 5-element subarray of the 
-// given array and divide the sum by ‘5’ to find the average. 
-
-function find_averages_of_subarrays(K, arr) {
+function findAveragesOfSubarrays(arr, k) {
     const result = [];
-    for (let i = 0; i < arr.length - K + 1; i++) {
-      // find sum of next 'K' elements
+    for (let i = 0; i < arr.length - k + 1; i++) {
+      // find sum of next 'k' elements
       sum = 0.0;
-      for (let j = i; j < i + K; j++) {
+      for (let j = i; j < i + k; j++) {
         sum += arr[j];
       }
-      result.push(sum / K); // calculate average
+      result.push(sum / k); // calculate average
     }
   
     return result;
   }
   
-  const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
-  console.log(`Averages of subarrays of size K: ${result}`);
+  const result = findAveragesOfSubarrays([1, 3, 2, 6, -1, 4, 1, 8, 2], 5);
+  console.log("Expect averages of subarrays of size k: 2.2,2.8,2.4,3.6,2.8");
+  console.log(`Averages of subarrays of size k: ${result}`);
 
 // Sliding Window approach:
 // The efficient way to solve this problem would be to visualize each subarray as a 
@@ -28,17 +28,17 @@ function find_averages_of_subarrays(K, arr) {
 // subarray, we will subtract the element going out of the window and add the element 
 // now being included in the sliding window. This will save us from going through the 
 // whole subarray to find the sum and, as a result, the algorithm complexity will 
-// reduce from O(N*K) to O(N).
+// reduce from O(n*k) to O(n).
 
-function find_averages_of_subarrays(K, arr) {
+function findAveragesOfSubarrays(arr, k) {
     const result = [];
     let windowSum = 0.0,
       windowStart = 0;
     for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
       windowSum += arr[windowEnd]; // add the next element
       // slide the window, we don't need to slide if we've not hit the required window size of 'k'
-      if (windowEnd >= K - 1) {
-        result.push(windowSum / K); // calculate the average
+      if (windowEnd >= k - 1) {
+        result.push(windowSum / k); // calculate the average
         windowSum -= arr[windowStart]; // subtract the element going out
         windowStart += 1; // slide the window ahead
       }
@@ -47,5 +47,6 @@ function find_averages_of_subarrays(K, arr) {
     return result;
   }
   
-  const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
-  console.log(`Averages of subarrays of size K: ${result}`);
+  const result = findAveragesOfSubarrays([1, 3, 2, 6, -1, 4, 1, 8, 2], 5);
+  console.log("Expect averages of subarrays of size k: 2.2,2.8,2.4,3.6,2.8");
+  console.log(`Averages of subarrays of size k: ${result}`);
