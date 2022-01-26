b4b32d17fa6a8869d1e879d0547c7feb36a3b0cc
Jaime Rogstad <jaimerogstad@gmail.com>
2022-01-25 20:39:05 -0500

    Add file

---
+++ fast_and_slow_pointers/palindrome_linked_list.rb
@@ -0,0 +1,16 @@
+# 234. Palindrome Linked List
+# Given the head of a singly linked list, return true if it is a palindrome.
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {Boolean}
+def is_palindrome(head)
+    
+end
