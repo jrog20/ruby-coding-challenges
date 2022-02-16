e519ddea41030f81fe537fcdb5b1257f150a8ed2
Jaime Rogstad <jaimerogstad@gmail.com>
2022-02-15 20:16:53 -0500

    Add notes

---
+++ in_place_reversal_of_linked_list/reverse_linked_list.rb
@@ -0,0 +1,33 @@
+# 206. Reverse Linked List
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Example 2:
+# Input: head = [1,2]
+# Output: [2,1]
+
+# Example 3:
+# Input: head = []
+# Output: []
+
+######################################################################################################
+# Iterative approach
+
+# We can traverse the list, changing the current node's next pointer to point to its previous element. 
+# Since a node does not have reference to its previous node, we need to first store its previous element. 
+# We also need another pointer to store the next node before changing the reference. 
+# Finally, return the new head reference.
+######################################################################################################
+# Recursive approach
+
+
+######################################################################################################
+
+def reverse_list(head)
+    
+end
+
