bd198a9d18206b67a9fa30d7076ee6b597a6a661
Jaime Rogstad <jaimerogstad@gmail.com>
2022-02-16 22:01:59 -0500

    Add iterative method

--- in_place_reversal_of_linked_list/reverse_linked_list.rb
+++ in_place_reversal_of_linked_list/reverse_linked_list.rb
@@ -15,19 +15,51 @@
 # Output: []
 
 ######################################################################################################
-# Iterative approach
 
-# We can traverse the list, changing the current node's next pointer to point to its previous element. 
-# Since a node does not have reference to its previous node, we need to first store its previous element. 
-# We also need another pointer to store the next node before changing the reference. 
-# Finally, return the new head reference.
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+# @param {ListNode} head
+# @return {ListNode}
+
+######################################################################################################
+# Iterative approach
+    # We can traverse the list, changing the current node's next pointer to point to its previous element. 
+    # Since a node does not have reference to its previous node, we need to first store its previous element. 
+    # We also need another pointer to store the next node before changing the reference. 
+    # Finally, return the new head reference.
+    # Edge cases = 
+
+    # Time complexity : O(n)
+    # Space complexity : O(1)
+######################################################################################################
+
+def reverse_list(head)
+    prev = nil
+    curr = head
+    while curr != nil
+        next_temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_temp
+    end
+    prev
+end
+
 ######################################################################################################
 # Recursive approach
 
 
 ######################################################################################################
 
-def reverse_list(head)
-    
+if __FILE__ == $PROGRAM_NAME
+    head = ListNode.new(1, ListNode.new(2, ListNode.new(3, ListNode.new(4, ListNode.new(5)))))
+    puts "Expecting [5, 4, 3, 2, 1]"
+    puts reverse_list(head)
 end
 
